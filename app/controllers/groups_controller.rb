aeb5b95a5ad94af1e02be4b34fa57689bcab26be
ken <aken@matoMac-mini-2.local>
2017-01-04 11:00:58 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -43,7 +43,7 @@ class GroupsController < ApplicationController
   private
 
   def find_group_and_check_permission
-    @group = Group.find(params[:Id])
+    @group = Group.find(params[:id])
 
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
